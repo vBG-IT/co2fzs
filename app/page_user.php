554f5404a964f0a39dc6fae936a39b0db0c37e6f
ZuraOfficial <jonas.barfknecht@gmail.com>
2018-05-06 12:58:19 +0200

    User Interface Update und Auslesen von Nutzerdaten

--- app/page_user.php
+++ app/page_user.php
@@ -1,13 +1,20 @@
 <?php
-$statement = $PDO->prepare("SELECT * FROM user WHERE name_first LIKE ? AND name_last LIKE ?")
-$statement = $PDO->execute(["Jonas"], ["Barfknecht"]);
-$statement->fetchAll();
+$user_statement = $PDO->prepare("SELECT * FROM user WHERE ID = :id");
+$user_statement->bindParam(':id', $USER); //USER ID
+$user_statement->execute();
+
+$user_o = $user_statement->fetchObject();
+$FIRSTNAME = $user_o->name_first;
+$LASTNAME = $user_o->name_last;
+$TEAM = $user_o->class;
+$DISTANCE = $user_o->default_distance;
+
 ?>
 
 <div class="row">
 	<div class="col-lg-12">
 		<h1 class="page-header">
-            <?php echo $VORNAME." ".$NACHNAME?>
+            <?php echo $FIRSTNAME." ".$LASTNAME?>
         </h1>
 	</div>
 </div>
@@ -53,12 +60,13 @@ $statement->fetchAll();
 				<div class="table-responsive">
 					<table class="table table-striped table-bordered table-hover">
 						<tbody>
-							<tr><th>Team</th><td><a href="index.php?p=team&id=<?php echo $member_team; ?>"><?php echo $member_team; ?></a></td></tr>
-							<tr><th>Punkte</th><td><?php echo ($member_punkte_3 * round(pow($distanz, 1))) + ($member_punkte_2 * round(pow($distanz, 1.8))) + ($member_punkte_1 * round(pow($distanz, 2))); ?></td></tr>
-							<tr><th>Schulweglänge</th><td><?php echo $distanz; ?>km</td></tr>
-							<tr><th>Distanz zu Fuß</th><td><?php echo $member_punkte_1 * $distanz; ?>km</td></tr>
-							<tr><th>Distanz mit dem Rad</th><td><?php echo $member_punkte_2 * $distanz; ?>km</td></tr>
-							<tr><th>Distanz per ÖPNV</th><td><?php echo $member_punkte_3 * $distanz; ?>km</td></tr>
+							<tr><th>Team</th><td><a href="index.php?p=team&id=<?php echo $TEAM; ?>"><?php echo $TEAM; ?></a></td></tr>
+							<tr><th>Schule</th><td><a href="index.php?p=schule&id=<?php echo $SCHOOL; ?>"><?php echo $SCHOOL; ?></a></td></tr>
+							<tr><th>Punkte</th><td><?php echo $POINTS?></td></tr>
+							<tr><th>Schulweglänge</th><td><?php echo $DISTANCE; ?>km</td></tr>
+							<tr><th>Distanz zu Fuß</th><td><?php echo $DISTANCE_FOOT; ?>km</td></tr>
+							<tr><th>Distanz mit dem Rad</th><td><?php echo $DISTANCE_CYCLE; ?>km</td></tr>
+							<tr><th>Distanz per ÖPNV</th><td><?php echo $DISTANCE_PT; ?>km</td></tr>
 						</tbody>
 					</table>
 				</div>
