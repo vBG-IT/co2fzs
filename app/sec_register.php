c21997c37ecd172c824e1512785acc6aeef93234
clerie <clemens@milchinsel.de>
2018-05-07 20:56:36 +0200

    Registrierungsworkflow überarbeitet

--- app/sec_register.php
+++ app/sec_register.php
@@ -1,13 +1,38 @@
+<?php
+if(!empty($_GET['class'])) {
+?>
 <form action="?sec=do-register-staff" method="POST">
-  <input type="hidden" name="school" value="<?php echo $_GET['school']; ?>" />
   <input type="hidden" name="class" value="<?php echo $_GET['class']; ?>" />
-  <input type="text" placeholder="Schule" disabled /> <a href="?sec=register">Ändern</a><br />
-  <input type="text" placeholder="Klasse" disabled /> <a href="?sec=register">Ändern</a><br />
+  <input type="text" placeholder="Klasse" value="<?php echo $_GET['class']; ?>" disabled /> <a href="?sec=register">Ändern</a><br />
   <input type="text" name="name_first" placeholder="Vorname" /><br />
   <input type="text" name="name_last" placeholder="Nachname" /><br />
   <input type="text" name="username" placeholder="Benutzername" /><br />
   <input type="password" name="password" placeholder="Passwort" /><br />
   <button type="submit">Anmelden</button><br />
 </form>
+<?
+}
+elseif (!empty($_GET['school'])) {
+  $statement = $PDO->prepare("SELECT * FROM class WHERE school = ? ORDER BY name");
+  $statement->execute([$_GET['school']]);
+  foreach ($statement->fetchAll() as $row) {
+    echo '<a href="'.$row['ID'].'">'.$row['name'].'</a><br />';
+  }
+}
+elseif (!empty($_GET['contest'])) {
+  $statement = $PDO->prepare("SELECT * FROM school WHERE contest = ? ORDER BY name");
+  $statement->execute([$_GET['contest']]);
+  foreach ($statement->fetchAll() as $row) {
+    echo '<a href="'.$row['ID'].'">'.$row['name'].'</a><br />';
+  }
+}
+else {
+  $statement = $PDO->prepare("SELECT * FROM contest ORDER BY name");
+  $statement->execute();
+  foreach ($statement->fetchAll() as $row) {
+    echo '<a href="'.$row['ID'].'">'.$row['name'].'</a><br />';
+  }
+}
+?>
 <a href="?sec=register-staff">Als Mitarbeiter registrieren</a><br />
 <a href="?sec=login">Anmelden</a><br />
